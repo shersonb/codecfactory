9ed30c632f00f1d187842a45e49e37d03b003810
Brian Sherson <caretaker82@hippocrates.shersonb.net>
2019-03-06 01:29:01 -0800

    Fixing unhook and allowedtype in numeralcodecs

--- codecfactory/numeralcodecs.py
+++ codecfactory/numeralcodecs.py
@@ -5,8 +5,8 @@ from codecset import CodecSet
 
 __all__ = ["uintcodec", "intcodec", "floatcodec", "rationalcodec", "realcodec"]
 
-uintcodec = RegExCodec(regex.compile("\d+"), hook=int, unhook=int.__str__, allowedtype=(int, long), name="Unsigned Integer Codec")
-intcodec = RegExCodec(regex.compile(r"[\+\-]?\d+"), hook=int, unhook=int.__str__, allowedtype=(int, long), name="Signed Integer Codec")
-floatcodec = RegExCodec(regex.compile(r"[\+\-]?(?:\d+\.\d*|\.\d+)(?:[Ee][\+\-]?\d+)?"), hook=float, unhook=float.__str__, allowedtype=(int, long, float), name="Float Codec")
+uintcodec = RegExCodec(regex.compile("\d+"), hook=int, unhook=str, allowedtype=(int, long), name="Unsigned Integer Codec")
+intcodec = RegExCodec(regex.compile(r"[\+\-]?\d+"), hook=int, unhook=str, allowedtype=(int, long), name="Signed Integer Codec")
+floatcodec = RegExCodec(regex.compile(r"[\+\-]?(?:\d+\.\d*|\.\d+)(?:[Ee][\+\-]?\d+)?"), hook=float, unhook=str, allowedtype=float, name="Float Codec")
 rationalcodec = RegExCodec(regex.compile(r"[\+\-]?\d+(?:/\d+)"), hook=fractions.Fraction, unhook="{0.numerator}/{0.denominator}".format, allowedtype=fractions.Fraction, name="Rational Codec")
 realcodec = CodecSet([floatcodec, rationalcodec, intcodec], name="Real Number Codec")
